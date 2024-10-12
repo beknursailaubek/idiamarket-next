6ac21223d1261828ee751ebc84457908e0cd0d2c
beknursailaubek <b3knursailaub3k@gmail.com>
2024-10-12 11:58:26 +0500

    contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,28 +1,146 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Contact.module.css";
 
 const ContactPage = () => {
+  const currentYear = new Date().getFullYear();
+  const yearsInMarket = currentYear - 2010;
+
   return (
     <div className="container">
       <div className={styles.body}>
         <h1 className="title">Контакты</h1>
-        <p className={styles.about}>IDIA Market является ведущим производителем складского, архивного и торгово-выставочного оборудования, имеет твердые позиции и занимает лидирующее положение на рынке металлической мебели. "IDIA Market" является успешным поставщиком ведущих производителей холодильного и торгового оборудования в Республике Казахстан. Компания предлагает современное торговое и POS оборудование. "IDIA Market" осуществляет не только продажу, но и комплексное внедрение автоматизации бизнеса. Производственная база компании позволяет выпускать оборудование в больших объемах, и вместе с тем совершенствовать изо дня в день свою продукцию, предлагать широкий спектр конструкций, моделей и цветов. Четкое планирование, высокая производительность, технологичность и возможность программирования систем позволяют выпускать продукцию высочайшего качества.</p>
+        <p className={styles.about}>IDIA Market — лидер на рынке складского, архивного и торгово-выставочного оборудования, а также поставщик холодильного и торгового оборудования в Казахстане. Компания предлагает современное торговое и POS-оборудование, внедряет комплексные решения по автоматизации бизнеса и работает на рынке уже {yearsInMarket} лет.</p>
 
-        <div className={styles.city}>
-          <div className={styles.info}>
-            <p className={styles.cityName}>г. Алматы, 050008, ул. Мынбаева 43 (уг.ул. Манаса)</p>
-            <Link className={`${styles.link} ${styles.email}`} href="mailto:zakaz@idiamarket.kz">
-              zakaz@idiamarket.kz
-            </Link>
-            <Link className={`${styles.link} ${styles.phone}`} href="tel:87273449900">
-              8 (727) 344-99-00
-            </Link>
-            <Link className={`${styles.link} ${styles.phone}`} href="tel:87012667700">
-              +7 (701) 266-77-00
-            </Link>
+        <div className={styles.cities}>
+          <div className={styles.city}>
+            <div className={styles.info}>
+              <p className={styles.cityName}>Алматы</p>
+
+              <div className={styles.address}>
+                <p className={styles.label}>Адрес</p>
+                <p className={styles.addressInfo}>г. Алматы, 050008, ул. Мынбаева 43 (уг.ул. Манаса)</p>
+              </div>
+
+              <div className={styles.schedule}>
+                <p className={styles.label}>Время работы</p>
+                <div className={styles.scheduleInfo}>
+                  <p>пн-пт с 9:00 до 18:00</p>
+                  <p>сб с 9:00 до 16:00</p>
+                </div>
+              </div>
+
+              <div className={styles.phone}>
+                <p className={styles.label}>Телефон</p>
+
+                <div className={styles.phoneInfos}>
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87012667700">
+                    +7 (701) 266-77-00
+                  </Link>
+
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87273449900">
+                    8 (727) 344-99-00
+                  </Link>
+                </div>
+              </div>
+
+              <div className={styles.email}>
+                <p className={styles.label}>Электронная почта</p>
+
+                <Link className={`${styles.link} ${styles.emailInfo}`} href="mailto:zakaz@idiamarket.kz">
+                  zakaz@idiamarket.kz
+                </Link>
+              </div>
+            </div>
+
+            <div className={styles.map}></div>
           </div>
 
-          <div className={styles.map}></div>
+          <div className={styles.city}>
+            <div className={styles.info}>
+              <p className={styles.cityName}>Астана</p>
+
+              <div className={styles.address}>
+                <p className={styles.label}>Адрес</p>
+                <p className={styles.addressInfo}>г. Астана, 010000, ул. Бейсекбаева 24/1, 2-этаж, бизнес центр DARA</p>
+              </div>
+
+              <div className={styles.schedule}>
+                <p className={styles.label}>Время работы</p>
+                <div className={styles.scheduleInfo}>
+                  <p>пн-пт с 9:00 до 18:00</p>
+                  <p>сб с 9:00 до 16:00</p>
+                </div>
+              </div>
+
+              <div className={styles.phone}>
+                <p className={styles.label}>Телефон</p>
+
+                <div className={styles.phoneInfos}>
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87015112200">
+                    +7 (701) 511-22-00
+                  </Link>
+
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87172279900">
+                    8 (7172) 27-99-00
+                  </Link>
+                </div>
+              </div>
+
+              <div className={styles.email}>
+                <p className={styles.label}>Электронная почта</p>
+
+                <Link className={`${styles.link} ${styles.emailInfo}`} href="mailto:zakaz@idiamarket.kz">
+                  astana@idiamarket.kz
+                </Link>
+              </div>
+            </div>
+
+            <div className={styles.map}></div>
+          </div>
+
+          <div className={styles.city}>
+            <div className={styles.info}>
+              <p className={styles.cityName}>Шымкент</p>
+
+              <div className={styles.address}>
+                <p className={styles.label}>Адрес</p>
+                <p className={styles.addressInfo}>г. Шымкент, ул. Мадели кожа 35/1, (уг.ул. Байтурсынова) 1-этаж, бизнес-центр BNK</p>
+              </div>
+
+              <div className={styles.schedule}>
+                <p className={styles.label}>Время работы</p>
+                <div className={styles.scheduleInfo}>
+                  <p>пн-пт с 9:00 до 18:00</p>
+                  <p>сб с 9:00 до 16:00</p>
+                </div>
+              </div>
+
+              <div className={styles.phone}>
+                <p className={styles.label}>Телефон</p>
+
+                <div className={styles.phoneInfos}>
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87012667700">
+                    +7 (701) 266-77-00
+                  </Link>
+
+                  <Link className={`${styles.link} ${styles.phoneInfo}`} href="tel:87273449900">
+                    8 (727) 344-99-00
+                  </Link>
+                </div>
+              </div>
+
+              <div className={styles.email}>
+                <p className={styles.label}>Электронная почта</p>
+
+                <Link className={`${styles.link} ${styles.emailInfo}`} href="mailto:zakaz@idiamarket.kz">
+                  zakaz@idiamarket.kz
+                </Link>
+              </div>
+            </div>
+
+            <div className={styles.map}></div>
+          </div>
         </div>
       </div>
     </div>
