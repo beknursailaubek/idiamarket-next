e4d1cf0b5294db6b5019a6692a4ab13afe540e27
beknursailaubek <b3knursailaub3k@gmail.com>
2024-10-17 15:08:35 +0500

    about page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="container flex flex-col items-center gap-[20px]">
+      <Image className="" src="/images/404_error.gif" width={560} height={420} alt="" />
+      <p className="">Упс! Я не нашел такую страницу</p>
+      <Link className=" text-sky-500 " href="/">
+        На главную страницу
+      </Link>
+    </div>
+  );
+}
