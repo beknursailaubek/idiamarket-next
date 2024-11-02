ab7a9cdeadb965944b809ebcdfa65115fe886d2e
beknursailaubek <b3knursailaub3k@gmail.com>
2024-11-02 11:20:02 +0500

    dynamic faq

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 import Image from "next/image";
 import { FaqData } from "@/types";
 import styles from "./Faq.module.css";
+import { useCityContext } from "@/hooks/useCityContext";
 
 const Faq = ({ data }: { data: FaqData }) => {
+  const { selectedCity } = useCityContext();
   const [expandedItems, setExpandedItems] = useState<number[]>([]);
 
   const toggleAnswer = (index: number) => {
     setExpandedItems((prev) => (prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]));
+  };
+
+  const checkCityInQuestion = (question: string) => {
+    return selectedCity ? question.replace("Алматы", selectedCity.title) : question;
   };
 
   return (
@@ -22,10 +28,10 @@ const Faq = ({ data }: { data: FaqData }) => {
         {data.list.map((item, index) => (
           <li key={index} className={styles.faqItem} itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
             <div className={styles.faqItemHeader} onClick={() => toggleAnswer(index)}>
-              <h4 className={styles.faqQuestion} itemProp="name">
-                {item.question}
+              <h4 className={`${item.answer ? styles.faqQuestion : styles.faqText}`} itemProp="name">
+                {checkCityInQuestion(item.question)}
               </h4>
-              <Image src="/images/icons/arrow-down.svg" width={20} height={20} alt="" className={`${expandedItems.includes(index) ? styles.iconRotated : ""}`} />
+              {item.answer && <Image src="/images/icons/arrow-down.svg" width={20} height={20} alt="" className={`${expandedItems.includes(index) ? styles.iconRotated : ""}`} />}
             </div>
             <div className={`${styles.faqAnswer} ${expandedItems.includes(index) ? "" : styles.faqAnswerHidden}`} itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
               <div itemProp="text" dangerouslySetInnerHTML={{ __html: item.answer }}></div>
