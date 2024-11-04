5e518f82250d2845e6db15f72753406556464ed7
beknursailaubek <b3knursailaub3k@gmail.com>
2024-11-04 15:55:41 +0500

    attributes fixed

--- src/components/ProductAttributes/ProductAttributes.tsx
+++ src/components/ProductAttributes/ProductAttributes.tsx
@@ -15,7 +15,7 @@ const ProductAttributes = ({ attributes }: AttributesProps) => {
     setIsAttributesHidden(!isAttributesHidden);
   };
 
-  console.log(attributes);
+  attributes = attributes.filter((item) => item.is_active);
 
   return (
     <div className={`${styles.attributes} ${isAttributesHidden ? styles.attributesHidden : null}`}>
@@ -23,35 +23,33 @@ const ProductAttributes = ({ attributes }: AttributesProps) => {
 
       <div className={styles.attributesList}>
         {attributes && attributes.length > 0
-          ? attributes
-              .filter((item) => item.is_active)
-              .map((group, index) => (
-                <div key={index} className={styles.attributesGroup}>
-                  <p className={styles.attributesGroupTitle}>{group.title}</p>
+          ? attributes.map((group, index) => (
+              <div key={index} className={styles.attributesGroup}>
+                <p className={styles.attributesGroupTitle}>{group.title}</p>
 
-                  {group.items && group.items.length > 0 ? (
-                    <ul className={styles.attributesItems}>
-                      {Object.entries(
-                        group.items
-                          .filter((item) => item.is_active)
-                          .reduce<Record<string, string[]>>((acc, item) => {
-                            // Group by title
-                            if (!acc[item.title]) {
-                              acc[item.title] = [];
-                            }
-                            acc[item.title].push(item.value);
-                            return acc;
-                          }, {})
-                      ).map(([title, values], idx: number) => (
-                        <li key={idx} className={styles.attribute}>
-                          <span className={styles.attributeTitle}>{title}</span>
-                          <span className={styles.attributeValue}>{values.join(", ")}</span>
-                        </li>
-                      ))}
-                    </ul>
-                  ) : null}
-                </div>
-              ))
+                {group.items && group.items.length > 0 ? (
+                  <ul className={styles.attributesItems}>
+                    {Object.entries(
+                      group.items
+                        .filter((item) => item.is_active)
+                        .reduce<Record<string, string[]>>((acc, item) => {
+                          // Group by title
+                          if (!acc[item.title]) {
+                            acc[item.title] = [];
+                          }
+                          acc[item.title].push(item.value);
+                          return acc;
+                        }, {})
+                    ).map(([title, values], idx: number) => (
+                      <li key={idx} className={styles.attribute}>
+                        <span className={styles.attributeTitle}>{title}</span>
+                        <span className={styles.attributeValue}>{values.join(", ")}</span>
+                      </li>
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+            ))
           : null}
       </div>
 
